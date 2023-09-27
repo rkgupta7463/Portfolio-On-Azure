c79a2581a8fd9f9cbadc7d0cf5f1eec438f83750
Rishu kumar gupta <96177171+rkgupta7463@users.noreply.github.com>
2023-09-27 14:52:24 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-c*w=l2)cj5^m-w8c_hpod8u*1y849n9!o%u+twpr=i&#$l314$
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['https://techknowtake.azurewebsites.net/','*']
+ALLOWED_HOSTS = ['*','https://techknowtake.azurewebsites.net']
 
 
 # Application definition
